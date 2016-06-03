a100cb3a250f894edf7fa7955437640c518470a2
rhykw <dev@rhykw.net>
2016-06-03 22:10:12 +0900

    modify test

--- test/mrb_unshare.rb
+++ test/mrb_unshare.rb
@@ -2,16 +2,6 @@
 ## Unshare Test
 ##
 
-assert("Unshare#hello") do
-  t = Unshare.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("Unshare#bye") do
-  t = Unshare.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("Unshare.hi") do
-  assert_equal("hi!!", Unshare.hi)
+assert("Unshare.unshare") do
+  assert_equal(0, Unshare.unshare(Unshare::CLONE_NEWUSER))
 end
