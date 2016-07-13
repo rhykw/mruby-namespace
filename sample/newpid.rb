f5dbc47b3174a117e26d5ff6e21c1322a650e45f
Uchio KONDO <udzura@pepabo.com>
2016-07-13 16:35:18 +0900

    Fix some

--- sample/newpid.rb
+++ sample/newpid.rb
@@ -1,9 +1,17 @@
 Namespace.unshare(Namespace::CLONE_NEWPID)
 
 p = Process.fork do
-  sleep 1
-  puts "exit"
+  Namespace.unshare(Namespace::CLONE_NEWNS)
+
+  system "mount --make-private /"
+  system "umount /proc"
+  system "mount -t proc proc /proc"
+  system "sleep 10 &"
+  system "sleep 10 &"
+  system "sleep 10 &"
   system "ps auxf"
 end
 
 p(Process.waitpid2 p)
+
+# system "mount -t proc proc /proc"
